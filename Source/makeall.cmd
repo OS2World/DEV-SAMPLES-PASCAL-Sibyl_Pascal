7f74c6a5ae070ca7831b871140c818cff7512420
Martin Iturbide <martin@os2world.com>
2014-08-13 11:04:02 -0500

    version 2.0.3.7 - sibylfp3.zip, sibyllibs3a.zip, sibylide.zip

---
+++ Source/makeall.cmd
@@ -0,0 +1,7 @@
+spc20.exe makeall.pas lib lib rtl;spcc;addon %1 %2 %3 -m -t1 -L -D -O0
+rem -$WA-
+
+
+
+
+
---
+++ Source/makeres.cmd
@@ -0,0 +1,18 @@
+cd spcc
+rcomp20 english.rc ..\lib
+rcomp20 buttons.rc ..\lib
+rcomp20 cursors.rc ..\lib
+rcomp20 dbctrls.rc ..\lib
+rcomp20 mmedia.rc ..\lib
+rcomp20 tabctrls.rc ..\lib
+cd ..
+cd addon
+rcomp20 CHECKLB.RC ..\lib
+rcomp20 COOLBAR.RC ..\lib
+rcomp20 DIROUTLN.RC ..\lib
+rcomp20 EDITLIST.RC ..\lib
+rcomp20 GLYPHBTN.RC ..\lib
+rcomp20 LED.RC ..\lib
+rcomp20 SEVEN.RC ..\lib
+rcomp20 SPIN.RC ..\lib
+cd ..
